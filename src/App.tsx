203dcc4229fbe0f73e93a19d563d29afa95dc397
lorescarpa <lorescarpa@gmail.com>
2024-11-15 18:59:36 +0100

    fix fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { Routes, Route } from 'react-router-dom';
+// import { Routes, Route } from 'react-router-dom';
 import './App.scss'
 import HomePage from './pages/homepage/HomePage';
 
 function App() {
   return (
     <div className="App">
-      {/* <HomePage /> */}
-      <Routes>
-        <Route path="/" element={<HomePage />}></Route>
-      </Routes>
+      <HomePage />
+      {/* <Routes>
+        <Route path="/home" element={<HomePage />}></Route>
+      </Routes> */}
     </div>
   );
 }
